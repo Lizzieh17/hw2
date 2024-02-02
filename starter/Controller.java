afaf2d8c7198e801c63108d6f8fb942ffe9833c7
Lizzie <150096267+Lizzieh17@users.noreply.github.com>
2024-02-02 14:24:14 -0600

    starter files

---
+++ starter/Controller.java
@@ -0,0 +1,16 @@
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Controller implements ActionListener
+{
+	public Controller()
+	{
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		System.out.println("Hey! I said not to push that button!");
+	}
+}
